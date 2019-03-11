4804791b2835856399299dde347c7309088e8a8d
jesusledesma15 <jesusproint998@gmail.com>
2019-03-11 18:37:17 +0100

    Creacion de la clase Centralita

---
+++ src/viviendadomotica/Centralita.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viviendadomotica;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Centralita {
+    private Estancia garaje;
+    private Estancia salon;
+    private Estancia dormitorio;
+    private Usuario user;
+    private LocalDate fechaInstalacion;
+    
+    private String id;     
+}
