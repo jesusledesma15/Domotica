1a5b498cafdcd6219a4821476d010af98fa7ee81
jesusledesma15 <jesusproint998@gmail.com>
2019-03-17 12:48:50 +0100

    Método menuDormitorio()

--- src/viviendadomotica/Vistas.java
+++ src/viviendadomotica/Vistas.java
@@ -89,4 +89,81 @@ public class Vistas {
         } while (!(numAction == 4));
         return null;
     }
+    
+    private static Comando menuDormitorio() {
+        Scanner teclado = new Scanner(System.in);
+        int numAction;
+        int optLuz, optPersiana, optCam;
+        do {
+            
+            System.out.println("--DORMITORIO--");
+            System.out.println("1. Luces");
+            System.out.println("2. Persianas");
+            System.out.println("3. Cámaras");
+            System.out.println("4. Salir");
+            System.out.print("Elige una acción: ");
+            numAction = teclado.nextInt();
+            switch (numAction) {
+                case 1:
+                    do {
+                        
+                        System.out.println("-- Luces del Dormitorio--");
+                        System.out.println("1. Consultar Luz");
+                        System.out.println("2. Encender Luz");
+                        System.out.println("3. Apagar Luz");
+                        System.out.println("4. Salir");
+                        optLuz = teclado.nextInt();
+                        switch (optLuz) {
+                            case 1:
+                                return Comando.CONSULTAR_LUZ_DORMITORIO;
+                            case 2:
+                                return Comando.ENCENDER_LUZ_DORMITORIO;
+                            case 3:
+                                return Comando.APAGAR_LUZ_DORMITORIO;
+                        }
+                    } while (!(optLuz == 4));
+                    break;
+                case 2:
+                    do {
+                        
+                        System.out.println("-- Persianas del dormitorio--");
+                        System.out.println("1. Consultar persiana");
+                        System.out.println("2. Subir persiana");
+                        System.out.println("3. Bajar persiana");
+                        System.out.println("4. Salir");
+                        optPersiana = teclado.nextInt();
+                        switch (optPersiana) {
+                            case 1:
+                                return Comando.CONSULTAR_PERSIANA_DORMITORIO;
+                            case 2:
+                                return Comando.SUBIR_PERSIANA_DORMITORIO;
+                            case 3:
+                                return Comando.BAJAR_PERSIANA_DORMITORIO;
+                        }
+                        break;
+                    } while (!(optPersiana == 4));
+                    break;
+                case 3:
+                    do {
+                        System.out.println("-- Cámaras del dormitorio--");
+                        System.out.println("1. Consultar Cámara");
+                        System.out.println("2. Apagar Cámara");
+                        System.out.println("3. Encender Cámara");
+                        System.out.println("4. Salir");
+                        optCam = teclado.nextInt();
+                        switch (optCam) {
+                            case 1:
+                                return Comando.CONSULTAR_CAMARA_DORMITORIO;
+                            case 2:
+                                return Comando.ENCENDER_CAMARA_DORMITORIO;
+                            case 3:
+                                return Comando.APAGAR_CAMARA_DORMITORIO;
+                        }
+                        
+                    } while (!(optCam == 4));
+                    break;
+            }
+        } while (!(numAction == 4));
+        return null;
+    }
 }
