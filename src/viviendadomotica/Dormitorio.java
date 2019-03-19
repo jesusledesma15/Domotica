8fb5d18c2da490da11421d1d173f70e45809f1dd
Salva <salvi205s@gmail.com>
2019-03-19 14:27:06 +0100

    Añade clase dormitorio

---
+++ src/viviendadomotica/Dormitorio.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domo;
+
+/**
+ *
+ * @author salva
+ */
+public class Dormitorio extends Habitacion{
+    
+    private Orientacion ori;
+
+    public Dormitorio(Orientacion ori, Luz luz, Persiana persiana, Camara camara, int m2) {
+        super(luz, persiana, camara, m2);
+        this.ori = ori;
+    }
+
+    public Orientacion getOri() {
+        return ori;
+    }
+
+    public void setOri(Orientacion ori) {
+        this.ori = ori;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"Dormitorio{" + "ori=" + ori + '}';
+    }
+    
+    
+}
