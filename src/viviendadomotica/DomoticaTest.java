6c5c9f3bba1dacf42f235a65fb264aea07df56e5
Jesus <jesusproint998@gmail.com>
2019-03-20 12:22:43 +0100

    Clase Main terminada

--- src/viviendadomotica/DomoticaTest.java
+++ src/viviendadomotica/DomoticaTest.java
@@ -5,11 +5,28 @@ package viviendadomotica;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Jesus
  */
 public class DomoticaTest {
-    
+
+    public static void main(String[] args) {
+        int contador = 0;
+
+        Centralita centr = new Centralita();
+        Usuario UsuAux = new Usuario();
+        
+        do {
+            UsuAux = Vistas.login();
+            contador++;
+
+        } while (contador < 5 && !UsuAux.equals(centr.getUser()));
+
+        if(centr.comprobarUsuario(UsuAux)){
+            Vistas.menu();
+        }else{
+            System.out.println("El usuario es erroneo");
+        }
+    }
 }
