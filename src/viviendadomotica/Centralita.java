6156cd7776474d560bac7517a5001d45544e63f2
Jesus <jesusproint998@gmail.com>
2019-03-20 12:23:19 +0100

    Método comprobar usuario y constructores

--- src/viviendadomotica/Centralita.java
+++ src/viviendadomotica/Centralita.java
@@ -12,11 +12,82 @@ import java.time.LocalDate;
  * @author Jesus
  */
 public class Centralita {
-    private Estancia garaje;
-    private Estancia salon;
-    private Estancia dormitorio;
-    private Usuario user;
-    private LocalDate fechaInstalacion;
     
-    private String id;     
+    private static Garaje garaje;
+    private static Salon salon;
+    private static Dormitorio dormitorio;
+    private static Usuario user;
+    private static LocalDate fechaInstalacion;
+    
+    private String id;   
+
+    public Centralita() {
+        garaje = new Garaje();
+        salon = new Salon();
+        dormitorio = new Dormitorio();
+        user = new Usuario("Jesus", "123J");
+        fechaInstalacion = fechaInstalacion;
+        id = id;
+    }
+    
+    public boolean comprobarUsuario(Usuario aux){
+        
+        return user.equals(aux);
+    }
+
+    public Garaje getGaraje() {
+        return garaje;
+    }
+
+    public void setGaraje(Garaje garaje) {
+        garaje = garaje;
+    }
+
+    public Salon getSalon() {
+        return salon;
+    }
+
+    public void setSalon(Salon salon) {
+        salon = salon;
+    }
+
+    public Dormitorio getDormitorio() {
+        return dormitorio;
+    }
+
+    public void setDormitorio(Dormitorio dormitorio) {
+        dormitorio = dormitorio;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        user = user;
+    }
+
+    public LocalDate getFechaInstalacion() {
+        return fechaInstalacion;
+    }
+
+    public void setFechaInstalacion(LocalDate fechaInstalacion) {
+        fechaInstalacion = fechaInstalacion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Centralita{" + "garaje=" + garaje + ", salon=" + salon + ", dormitorio=" + dormitorio + ", user=" + user + ", fechaInstalacion=" + fechaInstalacion + ", id=" + id + '}';
+    }
+    
+    
+    
 }
